ed38f8f74b810fa6fb965078a449172bfddc76f8
Izabelle Garcez <izabellegarcez33@gmail.com>
2025-06-04 15:12:13 -0300

    primeiro commit

---
+++ sistema-autolaudo/src/main/java/com/sistema/autolaudo/AutolaudoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.autolaudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolaudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolaudoApplication.class, args);
+	}
+
+}
